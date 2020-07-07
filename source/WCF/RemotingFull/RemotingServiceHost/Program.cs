382490c1c5fc8d6eb5bc43c49a73d630b3a34854
KUMAR Vimal <vimal.kumar@hexagonsi.com>
2020-07-07 19:38:01 +0530

    initial commit wcf

---
+++ source/WCF/RemotingFull/RemotingServiceHost/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ALL 3 for Remoting Hosting ability present inside System.Runtime.Remoting.dll
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace RemotingServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloRemotingService.HelloRemotingService remotingservice = new HelloRemotingService.HelloRemotingService();
+            TcpChannel channel = new TcpChannel(8080);
+            ChannelServices.RegisterChannel(channel);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(HelloRemotingService.HelloRemotingService),
+                "GetMessage", WellKnownObjectMode.Singleton); // SingleCall ==> can create single instance at a time and dont store the state of the object between multiple calls
+
+            Console.WriteLine(string.Format("Remoting Service Started...{0}",DateTime.Now));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ source/WCF/WCFFull/WCFPollingServiceEx/SampleService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFPollingServiceEx
+{
+    public class SampleService : ISampleService
+    {
+        public string GreetMe(Person person)
+        {
+            return string.Format("Greetings from Sample service Mr. {0}", person.Name);
+        }
+    }
+
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
